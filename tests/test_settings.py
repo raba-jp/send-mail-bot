3288ec81c329a15ceb8f29162fc1c0bbb60f1150
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-07-04 02:38:01 +0900

    Add settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,6 +1,6 @@
 import os
 from app import settings
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 
 
 class TestSettings:
@@ -10,6 +10,13 @@ class TestSettings:
     def test_env(self):
         expect = 'local'
         eq_(settings.env(), expect)
+
+    def test_env_local(self):
+        ok_(settings.env_local())
+
+    def test_env_prod(self):
+        os.environ['ENV'] = 'prod'
+        ok_(settings.env_prod())
 
     def test_local_secret_key(self):
         expect = '1234567890123456'
@@ -21,3 +28,8 @@ class TestSettings:
         os.environ['SECRET_KEY'] = expect
 
         eq_(settings.secret_key(), expect)
+
+    def test_slack_api_token(self):
+        expect = 'test_token'
+        os.environ['SLACK_API_TOKEN'] = expect
+        eq_(settings.slack_api_token(), expect)

74011af55c93d634066fa2cf54f304e4d6edc0bb
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-07-06 02:48:26 +0900

    Initialize bot

--- run.py
+++ run.py
@@ -1,27 +1,9 @@
-# coding: utf-8
-
-import os
-from flask import Flask
-from flask import render_template
-# from slackclient import SlackClient
-
-web = Flask(__name__)
+from slackbot.bot import Bot
 
 
 def main():
-    web.logger.info('Debug: {0}'.format(os.getenv('FLASK_DEBUG')))
-    SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
-    # slack_client = SlackClient(SLACK_API_TOKEN)
-
-    if SLACK_API_TOKEN is None:
-        raise RuntimeError()
-
-    web.run()
-
-
-@web.route("/")
-def index():
-    return render_template('index.html', message="Hello world")
+    bot = Bot()
+    bot.run()
 
 
 if __name__ == '__main__':
---
+++ slackbot_settings.py
@@ -0,0 +1,5 @@
+import os
+
+API_TOKEN = os.getenv('SLACK_API_TOKEN')
+
+default_reply = 'TODO: help message'
